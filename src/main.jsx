e008f4d481caa1e2e65e493cf580a9f05421b5f0
russellbanares322 <banaresrussell11@gmail.com>
2023-01-24 21:43:34 +0800

    feat: add media query to footer component

--- src/main.jsx
+++ src/main.jsx
@@ -24,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         draggable
         pauseOnHover
         theme="light"
+        style={{zIndex:'3'}}
         />
     </Router>
   </React.StrictMode>,
